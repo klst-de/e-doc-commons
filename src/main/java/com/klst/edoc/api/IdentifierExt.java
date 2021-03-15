d9fd78e9daeaba28617a5a3b6bbf5560f6ae289f
homebeaver <homebeaver@outlook.de>
2021-03-15 21:39:09 +0100

    IdText - #6

--- src/main/java/com/klst/edoc/api/IdentifierExt.java
+++ src/main/java/com/klst/edoc/api/IdentifierExt.java
@@ -2,11 +2,31 @@ package com.klst.edoc.api;
 
 /*
 
-Code Type = Identifier + VersionID of the schema/list
+IdentifierExt = Identifier + VersionID of the schema/list and idName
 used for Product Classification Class Code
 
  */
 public interface IdentifierExt extends Identifier {
+
+	/**
+	 * identification scheme identifier, f.i. "EN" (EAN)
+	 * @param id - the identification scheme shall be chosen from the entries in UNTDID 7143
+	 */
+	@Override
+	public void setSchemeIdentifier(String id);
+
+	/**
+	 * idText is the description for the SchemeIdentifier
+	 * <p>
+	 * example: content (aka Product Classification Class Code) is 4047247110051.
+	 * This is a code in the SchemeId UNTDID 7143 "EN". The idText for "EN" is
+	 * International Article Numbering Association (EAN):
+	 *  Number assigned to a manufacturer's product according to the International Article Numbering Association.
+	 * 
+	 * @param idText
+	 */
+	public void setIdText(String idText);
+	public String getIdText();
 
 	public void setVersionIdentifier(String versionID);
 	public String getVersionIdentifier();
