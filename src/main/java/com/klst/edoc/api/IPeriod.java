31533f0a3a5089cc4fd1e97d697b54363af59c61
homebeaver <homebeaver@outlook.de>
2021-03-27 23:58:20 +0100

    simpliFy interface IPeriod

--- src/main/java/com/klst/edoc/api/IPeriod.java
+++ src/main/java/com/klst/edoc/api/IPeriod.java
@@ -2,39 +2,22 @@ package com.klst.edoc.api;
 
 import java.sql.Timestamp;
 
-import com.klst.edoc.untdid.DateTimeFormats;
-
 /**
  * Period with Start and End Date
  * <p>
- * subclassed in e-invoice by
- * 
- * BG-14 INVOICING PERIOD
+ * subclassed in e-invoice, e-order by
+ * BG-14 PERIOD
  * and 
- * BG-26 INVOICE LINE PERIOD
+ * BG-26 LINE PERIOD
  */
 public interface IPeriod extends IPeriodFactory {
 	
-	public void setStartDate(Timestamp ts);
-	public Timestamp getStartDateAsTimestamp();
-	default void setStartDate(String ymd) {
-		if(ymd!=null) setStartDate(DateTimeFormats.ymdToTs(ymd));
-	}
+	// factory: createPeriod(Timestamp start, Timestamp end);
 	
-	public void setEndDate(Timestamp ts);
+//	public void setStartDate(Timestamp ts); // use factory
+	public Timestamp getStartDateAsTimestamp();
+	
+//	public void setEndDate(Timestamp ts);   // use factory
 	public Timestamp getEndDateAsTimestamp();
-	default void setEndDate(String ymd) {
-		if(ymd!=null) setEndDate(DateTimeFormats.ymdToTs(ymd));
-	}
-
-	default void setPeriod(String startYmd, String endYmd) {
-		setStartDate(DateTimeFormats.ymdToTs(startYmd));
-		setEndDate(DateTimeFormats.ymdToTs(endYmd));
-	}
-
-	default void setPeriod(Timestamp startTs, Timestamp endTs) {
-		setStartDate(startTs);
-		setEndDate(endTs);
-	}
 	
 }
